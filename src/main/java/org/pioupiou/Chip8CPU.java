1e34d7920ea1f9ad156e153f6a8f32a34da84f36
EdPiouPiou <hystadrion.allods.rp@gmail.com>
2025-05-30 18:56:44 +0200

    [BB8-CPU1] skeleton

---
+++ src/main/java/org/pioupiou/Chip8CPU.java
@@ -0,0 +1,26 @@
+package org.pioupiou;
+
+public class Chip8CPU {
+
+    private boolean drawFlag;
+
+    public boolean getDrawFlag() {
+        return drawFlag;
+    }
+
+    public void setDrawFlag(boolean drawFlag) {
+        this.drawFlag = drawFlag;
+    }
+
+    public void initialize(){
+
+    }
+
+    public void loadGame(String gameName){
+
+    }
+
+    public void emulate(){
+
+    }
+}
--- src/main/java/org/pioupiou/Main.java
+++ src/main/java/org/pioupiou/Main.java
@@ -1,7 +1,36 @@
 package org.pioupiou;
 
 public class Main {
+
+    private static Chip8CPU chip8;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //set up display and prep for user input
+        setupGraphics();
+        setupInput();
+
+        //init chip8 and load game
+        chip8.initialize();
+        chip8.loadGame("pong");
+
+        for(;;){
+            chip8.emulate();
+            //draw
+            if(chip8.getDrawFlag()){
+                drawGraphics();
+            }
+            //set pressed keys (method defined in Chip8 class)
+        }
     }
+
+    // methods
+    private static void drawGraphics(){
+
+    };
+    private static void setupGraphics(){
+
+    };
+    private static void setupInput(){
+
+    };
 }
