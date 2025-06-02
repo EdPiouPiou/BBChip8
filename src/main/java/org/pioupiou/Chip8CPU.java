8ba7aba8cd8fb4a701c67567d362ee9543942e58
EdPiouPiou <hystadrion.allods.rp@gmail.com>
2025-06-02 19:09:46 +0200

    [BB8-CPU2] opcodes work in progress

--- src/main/java/org/pioupiou/Chip8CPU.java
+++ src/main/java/org/pioupiou/Chip8CPU.java
@@ -1,16 +1,12 @@
 package org.pioupiou;
 
 public class Chip8CPU {
-
+    private int[] memory = new int[4096];
+    private boolean[][] display = new boolean[32][64];//binary
+    private String Vx = "V0";
+    private String Vy = "V0";
     private boolean drawFlag;
-
-    public boolean getDrawFlag() {
-        return drawFlag;
-    }
-
-    public void setDrawFlag(boolean drawFlag) {
-        this.drawFlag = drawFlag;
-    }
+    Opcodes opcodes = new Opcodes(display, memory, Vx, Vy);
 
     public void initialize(){
 
@@ -23,15 +19,22 @@ public class Chip8CPU {
     public void emulate(){
         //fetch opcode
         int programCounter = 0; //will be dependent of input
-        int[] memory = new int[4096]; //binary
             //data at a memory location is one byte, but an opcode is two, so we use pc and pc+1
         int locn = memory[programCounter]; //binary
         int locn1 = memory[programCounter+1]; //binary
             //merge both with a bitwise OR by shifting locn 8 bits, then merging
         int opcode = locn << 8 | locn1;
-        Opcodes.executeOpcode(Integer.toHexString(opcode));
+        opcodes.executeOpcode(Integer.toHexString(opcode));
 
         //decode opcode
         //execute opcode
+    }
+
+    public boolean getDrawFlag() {
+        return drawFlag;
+    }
+
+    public void setDrawFlag(boolean drawFlag) {
+        this.drawFlag = drawFlag;
     }
 }
--- src/main/java/org/pioupiou/Opcodes.java
+++ src/main/java/org/pioupiou/Opcodes.java
@@ -1,44 +1,180 @@
 package org.pioupiou;
 
 public class Opcodes {
-    public static void executeOpcode(String opcode){
+    private boolean[][] display;
+    private int[] memory; //binary
+    private String Vx;
+    private String Vy;
+    private String currentMemoryAddress;
+    private String adressI;
+    private int programCounter;
+    private int delayTimer;
+    private int soundTimer;
+    private int key;
+    private int Vf;
+
+    public Opcodes(boolean[][] display, int[] memory, String Vx, String Vy) {
+        this.display = display;
+    }
+
+    public void executeOpcode(String opcode){
         switch(opcode){
-            case "ONNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "00EO" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "00EE" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "1NNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "2NNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "3XNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "4XNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "5XY0" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "6XNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "7XNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY0" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY1" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY2" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY3" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY4" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY5" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY6" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XY7" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "8XYE" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "9XY0" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "ANNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "BNNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "CXNN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "DXYN" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "EX9E" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "EXA1" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX07" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX0A" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX15" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX18" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX1E" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX29" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX33" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX55" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            case "FX65" -> System.out.println("ERROR : NOT IMPLEMENTED");
-            default -> System.out.println("ERROR : UNKNOWN OPCODE");
+            case "ONNN":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "00EO":
+                this.clearDisplay();
+                break;
+            case "00EE":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "1NNN":
+                currentMemoryAddress = "0x" + opcode.substring(1);
+                break;
+            case "2NNN":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "3XNN":
+                Vx = "V" + opcode.charAt(1);
+                if(Vx == opcode.substring(2)){
+                    skip();
+                };
+                break;
+            case "4XNN":
+                Vx = "V" + opcode.charAt(1);
+                if(Vx != opcode.substring(2)){
+                    skip();
+                };
+                break;
+            case "5XY0":
+                Vx = "V" + opcode.charAt(1);
+                Vy = "V" + opcode.charAt(2);
+                if(Vx == Vy){
+                    skip();
+                };
+                break;
+            case "6XNN":
+                Vx = opcode.substring(2);
+                break;
+            case "7XNN":
+                Vx = "V" + opcode.charAt(1);
+                if(Vx != "VF"){
+                    Vx = Integer.toHexString(Integer.parseInt(Vx, 16) + Integer.parseInt(opcode.substring(2),16));
+                }
+                break;
+            case "8XY0":
+                Vx = "V" + opcode.charAt(2);
+                break;
+            case "8XY1":
+                Vx = "V" + opcode.charAt(1);
+                Vx = Integer.toHexString(Integer.parseInt(Vx, 16) | Integer.parseInt(opcode.substring(2),16));
+                break;
+            case "8XY2":
+                Vx = "V" + opcode.charAt(1);
+                Vx = Integer.toHexString(Integer.parseInt(Vx, 16) & Integer.parseInt(opcode.substring(2),16));
+                break;
+            case "8XY3":
+                Vx = "V" + opcode.charAt(1);
+                Vx = Integer.toHexString(Integer.parseInt(Vx, 16) ^ Integer.parseInt(opcode.substring(2),16));
+                break;
+            case "8XY4":
+                Vx = "V" + opcode.charAt(1);
+                Vy = "V" + opcode.charAt(2);
+                int tempResultOverflow = Integer.parseInt(Vx, 16) + Integer.parseInt(Vy,16); //used to check for overflow
+                if(tempResultOverflow < 0 ){ //check for overflow
+                    Vf = 1;
+                }
+                else{
+                    Vf = 0;
+                }
+                break;
+            case "8XY5":
+                Vx = "V" + opcode.charAt(1);
+                Vy = "V" + opcode.charAt(2);
+                if(Integer.parseInt(Vx, 16) >= Integer.parseInt(Vy, 16)){ //check for underflow
+                    Vf = 1;
+                }
+                else{
+                    Vf = 0;
+                }
+                break;
+            case "8XY6":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "8XY7":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "8XYE":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "9XY0":
+                Vx = "V" + opcode.charAt(1);
+                Vy = "V" + opcode.charAt(2);
+                if(Vx != Vy){
+                    skip();
+                };
+                break;
+            case "ANNN":
+                adressI = opcode.substring(1);
+                break;
+            case "BNNN":
+                programCounter = Integer.parseInt("V0", 16) + Integer.parseInt(opcode.substring(1),16);
+                break;
+            case "CXNN":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "DXYN":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "EX9E":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "EXA1":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "FX07":
+                Vx = Integer.toHexString(getDelay());
+                break;
+            case "FX0A":
+                Vx = Integer.toHexString(getKey());
+                break;
+            case "FX15":
+                delayTimer = Integer.parseInt(Vx, 16);
+                break;
+            case "FX18":
+                soundTimer = Integer.parseInt(Vx, 16);
+                break;
+            case "FX1E":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "FX29":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "FX33":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "FX55":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
+            case "FX65":
+                System.out.println("ERROR : NOT IMPLEMENTED");
+                break;
         }
+    }
+
+    private void clearDisplay(){
+        display = new boolean[32][64];
+    }
+
+    private void skip(){
+
+    }
+
+    private int getDelay(){
+        return 0;
+    }
+
+    private int getKey(){
+        return 0;
     }
 }
